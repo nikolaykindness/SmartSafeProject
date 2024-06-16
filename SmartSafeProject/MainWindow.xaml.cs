31b885ae27d5e34b256131d4fec7a5ccc4b0bc68
Nikolay Kindness <nikolaykindness@mail.ru>
2024-06-16 22:04:48 +0300

    Добавление БД

--- SmartSafeProject/MainWindow.xaml.cs
+++ SmartSafeProject/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System;
+using System.Windows;
+using FirebirdSql.Data.FirebirdClient;
 
 namespace SmartSafeProject
 {
@@ -10,6 +13,24 @@ namespace SmartSafeProject
         public MainWindow()
         {
             InitializeComponent();
+
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+
+            FbConnectionStringBuilder bld = new FbConnectionStringBuilder();
+            //bld.Charset = "NONE";
+            //bld.DataSource = "localhost";
+            bld.Database = $@"{projectDirectory}\SMARTSAFE.FDB";
+            bld.UserID = "";
+            bld.Password = "";
+            bld.WireCrypt = FbWireCrypt.Enabled;
+            string connStr = bld.ConnectionString;
+
+            string connectionString = $@"XpoProvider=Firebird;DataSource=localhost;User=;Password=;Database={projectDirectory}\PYGG.FDB;ServerType=1;Charset=NONE";
+
+            //FbConnection connecting = new FbConnection($@"User = admin; Password = admin; Database = {projectDirectory}\SMARTSAFE.FDB");
+            FbConnection connecting = new FbConnection(connStr);
+            connecting.Open();
         }
     }
 }
