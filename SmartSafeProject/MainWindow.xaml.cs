7494121098e4936ce4277eeb9975474b9e22c886
Nikolay Kindness <nikolaykindness@mail.ru>
2024-06-16 22:38:15 +0300

    Правильная реализация подключения.

--- SmartSafeProject/MainWindow.xaml.cs
+++ SmartSafeProject/MainWindow.xaml.cs
@@ -17,19 +17,9 @@ namespace SmartSafeProject
             string workingDirectory = Environment.CurrentDirectory;
             string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
 
-            FbConnectionStringBuilder bld = new FbConnectionStringBuilder();
-            //bld.Charset = "NONE";
-            //bld.DataSource = "localhost";
-            bld.Database = $@"{projectDirectory}\SMARTSAFE.FDB";
-            bld.UserID = "";
-            bld.Password = "";
-            bld.WireCrypt = FbWireCrypt.Enabled;
-            string connStr = bld.ConnectionString;
-
-            string connectionString = $@"XpoProvider=Firebird;DataSource=localhost;User=;Password=;Database={projectDirectory}\PYGG.FDB;ServerType=1;Charset=NONE";
-
-            //FbConnection connecting = new FbConnection($@"User = admin; Password = admin; Database = {projectDirectory}\SMARTSAFE.FDB");
-            FbConnection connecting = new FbConnection(connStr);
+            string connectionString = $@"User=sysdba;Password=masterkey;Database={projectDirectory}\SMARTSAFE.FDB;DataSource=127.0.0.1;Port=3050;Dialect=3;Charset=NONE;Role=;Connection lifetime=15;Pooling=true;MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;";
+            
+            FbConnection connecting = new FbConnection(connectionString);
             connecting.Open();
         }
     }
