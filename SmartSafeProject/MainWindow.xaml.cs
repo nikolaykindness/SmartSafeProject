e281fdf00e124d035e5fed94ba69cfad01a3f770
Micanit <micanit04@mail.ru>
2024-06-17 04:09:00 +0300

    Создал поиск файла

--- SmartSafeProject/MainWindow.xaml.cs
+++ SmartSafeProject/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Windows;
 using FirebirdSql.Data.FirebirdClient;
+using Microsoft.Win32;
+using System.Windows.Controls;
 
 namespace SmartSafeProject
 {
@@ -14,13 +16,40 @@ namespace SmartSafeProject
         {
             InitializeComponent();
 
+
+            //Подключени к БД
             string workingDirectory = Environment.CurrentDirectory;
             string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
 
-            string connectionString = $@"User=sysdba;Password=masterkey;Database={projectDirectory}\SMARTSAFE.FDB;DataSource=127.0.0.1;Port=3050;Dialect=3;Charset=NONE;Role=;Connection lifetime=15;Pooling=true;MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;";
+            string connectionString = $@"User=sysdba;Password=masterkey;Database={projectDirectory}\SMARTSAFE.FDB;DataSource=localhost;Port=3050;Dialect=3;Charset=NONE;Role=;Connection lifetime=15;Pooling=true;MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;";
             
             FbConnection connecting = new FbConnection(connectionString);
             connecting.Open();
+
+
+
+            //открытие формы для поиска файла
+            var dialogSearchImages = new Microsoft.Win32.OpenFileDialog();
+            dialogSearchImages.InitialDirectory = $@"{projectDirectory}\files\";
+            dialogSearchImages.DefaultExt = ".JPEG"; 
+            dialogSearchImages.Filter = "Image Files|*.BMP;*.JPG;*.GIF;*.JPEG";
+
+            bool? result = dialogSearchImages.ShowDialog();
+
+            if (result == true)
+            {
+                //Чтение данных с файла
+                string fileFullPath = dialogSearchImages.FileName;
+                FileInfo fileInfo = new FileInfo(fileFullPath);
+                if (fileInfo.Exists)
+                {
+                    //TODO: Надо забрать данные с файла
+                    MessageBox.Show($"Имя файла: {fileInfo.Name}");
+                    MessageBox.Show($"Время создания: {fileInfo.CreationTime}");
+                    MessageBox.Show($"Размер: {fileInfo.Length}");
+                }
+            }
+
         }
     }
 }
